2a314c74ba7d4f675c1513778c3713043a516407
Amit Breiman <crimson.breiman@gmail.com>
2024-05-08 02:06:42 +0300

    1

--- CardGameEngine/Data Classes/Card.cs
+++ CardGameEngine/Data Classes/Card.cs
@@ -1,16 +1,15 @@
 ﻿
 namespace CardGameEngine
 {
-    public abstract class Card : IComparable<Card>, IEquatable<Card>
+    public class Card : IComparable<Card>, IEquatable<Card>
     {
         public int ID { get; init; }
 
-        public Card(int id)
+        public Card() { }
+        public Card(int id) : this()
         {
             ID = id;
         }
-
-
 
 
         public int CompareTo(Card? other)
@@ -20,6 +19,61 @@ namespace CardGameEngine
         public bool Equals(Card? other)
         {
             return ID.Equals(other?.ID);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            return this.Equals((Card?)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
+
+        public static bool operator ==(Card left, Card right)
+        {
+            if (ReferenceEquals(left, null))
+            {
+                return ReferenceEquals(right, null);
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Card left, Card right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator <(Card left, Card right)
+        {
+            return ReferenceEquals(left, null) ? !ReferenceEquals(right, null) : left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(Card left, Card right)
+        {
+            return ReferenceEquals(left, null) || left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(Card left, Card right)
+        {
+            return !ReferenceEquals(left, null) && left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(Card left, Card right)
+        {
+            return ReferenceEquals(left, null) ? ReferenceEquals(right, null) : left.CompareTo(right) >= 0;
         }
     }
 }
